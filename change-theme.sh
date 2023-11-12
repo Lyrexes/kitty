b3b2df74e1a340124c262b7b31a98b260efd994a
Ruben <steamkonto60@gmail.com>
2023-11-12 19:32:07 +0100

    init commit

---
+++ change-theme.sh
@@ -0,0 +1,3 @@
+cd ~/.config/kitty
+unlink theme.conf
+ln -s ./kitty-themes/themes/$1.conf ~/.config/kitty/theme.conf
---
+++ term-white-mode.sh
@@ -0,0 +1,4 @@
+cd ~/.config/kitty
+unlink theme.conf
+ln -s ./kitty-themes/themes/Github.conf ~/.config/kitty/theme.conf
+
